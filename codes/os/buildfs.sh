f16d45df8d9d9eb76955ff4ae38c8bf2b2fc5072
LoanCold <850323576@qq.com>
2021-05-15 17:12:25 +0800

    Test Env constructed, now start testing

---
+++ codes/os/buildfs.sh
@@ -0,0 +1,22 @@
+U_FAT32_DIR="../fat32-fuse"
+U_FAT32="${U_FAT32_DIR}/fat32.img"
+
+if test -e ${U_FAT32_DIR}/fs
+then 
+    echo "already have fs"
+else
+    mkdir ${U_FAT32_DIR}/fs
+fi
+
+sudo mount ${U_FAT32} ${U_FAT32_DIR}/fs
+sudo rm ${U_FAT32_DIR}/fs/*
+for programname in $(ls ../user/riscv64)
+do 
+    sudo cp ../user/riscv64/$programname ${U_FAT32_DIR}/fs/"testsuites_$programname"
+done
+for programname in $(ls ../user/src/bin)
+do
+    sudo cp ../user/target/riscv64gc-unknown-none-elf/release/${programname%.rs} ../fat32-fuse/fs/${programname%.rs}
+done
+
+sudo umount ${U_FAT32_DIR}/fs
--- codes/os/src/fs/inode.rs
+++ codes/os/src/fs/inode.rs
@@ -116,7 +116,7 @@ lazy_static! {
 */
 pub fn list_apps() {
     println!("/**** APPS ****");
-    for app in ROOT_INODE.ls_lite().unwrap() {
+    for app in ROOT_INODE.ls().unwrap() {
         println!("{}", app.0);
     }
     println!("**************/")
