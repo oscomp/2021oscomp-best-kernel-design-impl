61ed800fa6ed87477005f145634032bb45c0d849
HappyEric <wuyize18@mails.ucas.ac.cn>
2021-05-24 18:48:14 -0700

    sd_read17

--- drivers/sdcard/sdcard.c
+++ drivers/sdcard/sdcard.c
@@ -205,10 +205,12 @@ static uint8 sd_get_csdregister(SD_CSD *SD_csd)
 	sd_send_cmd(SD_CMD9, 0, 0);
 	/*!< Wait for response in the R1 format (0x00 is no errors) */
 	if (sd_get_response() != 0x00) {
+		printk("here\n"); while(1);
 		sd_end_cmd();
 		return 0xFF;
 	}
 	if (sd_get_response() != SD_START_DATA_SINGLE_BLOCK_READ) {
+		printk("there\n"); while(1);
 		sd_end_cmd();
 		return 0xFF;
 	}
