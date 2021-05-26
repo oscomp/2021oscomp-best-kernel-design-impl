b63c2ce2fc595754395a444bea8f4cef67e3bcb0
HappyEric <wuyize18@mails.ucas.ac.cn>
2021-05-26 03:51:02 -0700

    day7

--- include/user_programs.h
+++ include/user_programs.h
@@ -1,16 +1,12 @@
 
 extern unsigned char _elf___test_shell_elf[];
 int _length___test_shell_elf;
-extern unsigned char _elf___test_clone_elf[];
-int _length___test_clone_elf;
-extern unsigned char _elf___test_uname_elf[];
-int _length___test_uname_elf;
 typedef struct ElfFile {
   char *file_name;
   unsigned char* file_content;
   int* file_length;
 } ElfFile;
 
-#define ELF_FILE_NUM 3
-extern ElfFile elf_files[3];
+#define ELF_FILE_NUM 1
+extern ElfFile elf_files[1];
 extern int get_elf_file(const char *file_name, unsigned char **binary, int *length);
