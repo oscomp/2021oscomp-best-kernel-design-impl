8451c1554357eca1c07c23c4b29f90bc466b2cfe
retrhelo <artyomliu@foxmail.com>
2021-05-28 13:38:59 +0800

    modify clock freq

--- kernel/include/param.h
+++ kernel/include/param.h
@@ -19,7 +19,8 @@
 #ifndef QEMU 
 //#define INTERVAL     (390000000 / 200) // timer interrupt interval
 // assume that K210 system freq is 26MHz
-#define CLK_FREQ 		26000000
+//#define CLK_FREQ 		26000000
+#define CLK_FREQ 		12500000 
 #else 
 // QEMU system freq is 12.5MHz 
 #define CLK_FREQ 		12500000
